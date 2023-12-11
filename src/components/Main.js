f2713a14c3fa54930a565a73775250f50cfce5e5
wsantl <wsantlba@hotmail.com>
2023-12-11 14:56:49 -0300

    component de estado e estilizacao css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import './Main.css';
 
 export default class Main extends Component {
-    constructor(props){
-        super(props);
+    
+    state = {
+        novaTarefa:'',
+    };
 
-        this.state = {
-
-        };
+    handleChange = (e) => {
+        this.setState({
+            novaTarefa: e.target.value,
+        });
     }
 
-
     render() {
-        return <h1> Meu component com estado </h1>
+        const {novaTarefa} = this.state;
+
+        return (
+            <div className='main'>
+                 <h1> Lista de tarefas </h1>
+
+                 <form action="#">
+                    <input onChange={this.handleChange} type="text"/>
+                    <button type="submit"> Enviar </button>
+                 </form>
+            </div>
+        )
     }
 }

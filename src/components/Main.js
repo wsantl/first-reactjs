096984b9307b5fc8d0f339b209535f488e912b6a
wsantl <wsantlba@hotmail.com>
2023-12-11 13:18:54 -0300

    create component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Main extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+
     render() {
         return <h1> Meu component com estado </h1>
     }
